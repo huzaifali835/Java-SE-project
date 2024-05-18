0334406a6aebad517f0d1e4d1c4bc145d1fbf488
huzaifali835 <huzaifali795@gmail.com>
2024-05-18 20:01:39 +0500

    define 8 new Functions in Main.Java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main{
             System.out.println("/nChoose an opearation");
             System.out.println("1. Bubble Sort");
             System.out.println("2. Factorial");
+            System.out.println("3. Binary Search");
+            System.out.println("4. GCD");
+            System.out.println("5. LCM");
+            System.out.println("6. Anagram Checker");
+            System.out.println("7. Armstrong Number");
+            System.out.println("8. Fibonacci");
+            System.out.println("9. Sum of Digits");
+            System.out.println("10. Factorial");
             System.out.println("0. Exit");
 
             int userInput = scanner.nextInt();
